73ae9b37b9c5345c2ab36230b718fe1a949154e7
Paige <Pbriggscreative@gmail.com>
2022-04-12 23:00:55 -0400

    intial commit

---
+++ loops.js
@@ -0,0 +1,34 @@
+//  for (let i = 1; i <= 10; i++) {
+//      console.log(i);
+//  }
+
+//  for (let i = 10; i >= 1; i--) {
+//      console.log(i);
+//  }
+
+//  let i = 10
+
+//  while ( i <= 10){
+//      console.log(i);
+//      i++;
+//  }
+
+//  while ( i >= 1){
+//      console.log(i);
+//      i--;
+//  }
+
+//  let d = 1;
+
+//  do{
+//      console.log(d);
+//      d++;
+//  } while (d<=10)
+
+
+//  let d = 10;
+
+//  do{
+//      console.log(d);
+//      d--;
+//  } while (d>=1)
