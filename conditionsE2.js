338852e5247bef3f82e44d9e63301bb15c1b4c09
Paige <Pbriggscreative@gmail.com>
2022-04-12 22:36:37 -0400

    intial commit

---
+++ conditionsE2.js
@@ -0,0 +1,13 @@
+// conditionals exercise 1
+ var actualTemp = 80
+ var desiredTemp = 75
+
+ if (actualTemp < desiredTemp) {
+    console.log("Run heat");
+ 
+   } else if (actualTemp > desiredTemp) {
+    console.log("Run A/C");
+
+ } else {
+      console.log("Standby");
+ }
